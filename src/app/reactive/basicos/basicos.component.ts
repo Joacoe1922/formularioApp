b116410d67662e2bdc53634db8e34294cadc14a3
Joaquin Nicolas Coelho <jnicolas@shs.local>
2022-07-05 13:21:51 +0200

    2nd commit (terminado)

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre'     : new FormControl('RTX 4080Ti'),
+  //   'precio'     : new FormControl(1500),
+  //   'existencias': new FormControl(5)
+  // });
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.fb.group({
+    nombre     : [ , [ Validators.required, Validators.minLength(3) ] ],
+    precio     : [ , [ Validators.required, Validators.min(0) ] ],
+    existencias: [ , [ Validators.required, Validators.min(0) ] ]
+  })
+
+  constructor( private fb: FormBuilder ) {}
+  
+  campoEsValido( campo: string ) {
+    return this.miFormulario.controls?.[ campo ]?.errors 
+            && this.miFormulario.controls?.[ campo ]?.touched;
   }
+
+  guardar() {
+
+    if ( this.miFormulario.invalid ) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.miFormulario.value );
+    this.miFormulario.reset();
+  }
+
 
 }
